06cbda2f30f7163644d57cc0c17038454c4adea0
Jeffrey Davis <itechify@gmail.com>
2025-08-09 16:53:42 -0700

    Update import statement for Vercel Analytics component to use the correct package path

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import './global.css'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
-import {Analytics} from '@vercel/analytics/next'
+import {Analytics} from '@vercel/analytics/react'
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {BrowserRouter} from 'react-router'
